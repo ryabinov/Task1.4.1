24908a171c47f6e366c7b1cf22d8617a1cce5d88
RyabinovDmitrii <rojoc@yandex.ru>
2020-11-19 17:25:58 +0300

    изменения 1.2

---
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -0,0 +1,133 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.*;
+
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserDaoHibernateImpl extends Util implements UserDao {
+
+    public UserDaoHibernateImpl() {
+
+    }
+
+    @Override
+    public void createUsersTable() throws SQLException {
+
+        Session session = getSessionFactory().openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE IF NOT EXISTS user" +
+                    "(id INT PRIMARY KEY AUTO_INCREMENT," +
+                    "name VARCHAR(50)," +
+                    "lastName VARCHAR(50)," +
+                    "age INT)").executeUpdate();
+            transaction.commit();
+            System.out.println("Таблица добавлена");
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void dropUsersTable() {
+
+        Session session = getSessionFactory().openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = session.beginTransaction();
+            session.createSQLQuery("DROP TABLE IF EXISTS user").executeUpdate();
+            transaction.commit();
+            System.out.println("Таблица удалена");
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void saveUser(String name, String lastName, byte age) {
+
+        Session session = getSessionFactory().openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = session.beginTransaction();
+            session.persist(new User(name, lastName, age));
+            transaction.commit();
+            System.out.println("User добавлен в таблицу!!!" + "\n");
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void removeUserById(long id) {
+
+        Session session = getSessionFactory().openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = session.beginTransaction();
+            User user = (User) session.load(User.class, id);
+            session.delete(user);
+            transaction.commit();
+            System.out.println(user + " удалет по id: " + id + "\n");
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        List<User> userList = null;
+
+        Session session = getSessionFactory().openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = session.beginTransaction();
+            userList = session.createQuery("from User").list();
+            transaction.commit();
+            System.out.println("Users находящиеся в таблицы: " + userList + "\n");
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        }
+
+        return userList;
+    }
+
+
+    @Override
+    public void cleanUsersTable() {
+
+        Session session = getSessionFactory().openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = session.beginTransaction();
+            List<User> userList = session.createQuery("from User").list();
+            for (User u : userList) {
+                session.delete(u);
+                System.out.println(u + " Удален!" + "\n");
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+}
+
+
+//://www.journaldev.com/3531/spring-mvc-hibernate-mysql-integration-crud-example-tutorial
